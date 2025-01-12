a777e176f5f1c636b38babca97131bc546a72571
Felix Mennen <xyron1996@gmail.com>
2025-01-12 18:03:21 +0100

    add import script

---
+++ util/testdata/pull.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+QUERY="
+    select distinct on (id)
+        uuid as id,
+        timestamp,
+        event as event_type,
+        person_id as user_id,
+        properties,
+    from posthog.events
+    into outfile '/events.csv.gz'
+    TRUNCATE
+    COMPRESSION 'gzip'
+    format CSVWithNames;
+"
+
+ssh root@ph.fmennen.de "docker container exec posthog-clickhouse-1 clickhouse-client --query \"$QUERY\""
+ssh root@ph.fmennen.de "docker container cp posthog-clickhouse-1:/events.csv.gz /events.csv.gz"
+
+rm -f events.csv.gz
+
+rsync --progress -e ssh root@ph.fmennen.de:/events.csv.gz .
+
+echo "Download complete. Importing..."
+
+QUERY2="
+    copy events from 'events.csv.gz';
+"
+
+duckdb ../../backend/_data/analytics_test.db "$QUERY2"
+
