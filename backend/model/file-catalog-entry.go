f6eb3266331b0b88c0082d5d46b759e53a778de8
Felix Mennen <xyron1996@gmail.com>
2025-02-06 21:52:13 +0100

    fix pk

--- backend/model/file-catalog-entry.go
+++ backend/model/file-catalog-entry.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type FileCatalogEntry struct {
-	Name       string     `gorm:"primarykey,size:512;not null" json:"name"`
+	Name       string     `gorm:"type:varchar(512);primary_key;;not null" json:"name"`
 	Start      *time.Time `json:"start" gorm:"not null"`
 	End        *time.Time `json:"end"`
 	ValidUntil *time.Time `json:"validUntil"`
