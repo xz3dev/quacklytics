2261aeea80c4aa86c558bc05912dc493475ecd71
Felix Mennen <xyron1996@gmail.com>
2025-03-02 10:51:48 +0100

    add another test

--- backend/events/process_event_person_test.go
+++ backend/events/process_event_person_test.go
@@ -133,3 +133,44 @@ func TestOverwritingPropertiesBasedOnTimestamp(t *testing.T) {
 	assert.NotNil(t, person)
 	assert.DeepEqual(t, person.Properties, expectedProperties)
 }
+
+// Test that events without a distinct ID are silently dropped.
+func TestEventWithoutDistinctIdIsDropped(t *testing.T) {
+	s := testsetup.Setup(t)
+	defer s.Dispose()
+
+	testEvents := []*model.EventInput{
+		{
+			EventType: "should_be_dropped",
+			Timestamp: time.Now(),
+		},
+		{
+			EventType:  "test_type",
+			DistinctId: "id_1",
+			Timestamp:  time.Now(),
+		},
+	}
+
+	p := ProjectProcessor{
+		projectID:  "test_runner",
+		db:         s.ProjectDB,
+		dbd:        &s.DuckDB,
+		eventQueue: make(chan *model.EventInput, 1),
+	}
+
+	existingPersons, err := p.getExistingPersons(testEvents)
+	assert.NoError(t, err)
+
+	e := EventProcessor{
+		Input: EventProcessorInput{
+			Events:          testEvents,
+			ExistingPersons: existingPersons,
+		},
+	}
+
+	err = e.Process()
+	assert.NoError(t, err)
+	// Verify that only the event with a distinct ID is processed
+	assert.Equal(t, len(e.Output.NewEvents), 1)
+	assert.Equal(t, e.Output.NewEvents[0].DistinctId, "id_1")
+}
